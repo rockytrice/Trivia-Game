deb7278ae5fabf6466b3c67ad028761118bc09c8
Roderick Trice <Rodericktrice@Rodericks-MacBook-Pro.local>
2018-07-26 17:17:20 -0400

    configuring start screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,22 @@
-// variable for correct and wrong answers
-var right = 0;
-var wrong = 0;
-
-
-
-
+// global variables
+// variables for correct and wrong answers
+var correct = 0;
+var incorrect = 0;
+// testing purposes
+console.log("hello");
+// variable that holds the initial starting point for the timer
 var count = 60;
-
+// clock counts 
 var counter = setInterval(timer, 1000);
 
+var clockRunning = false;
+
+
+// timer function
 function timer() {
     $("#display-timer").text(count);
-    count = count - 1;
-    if (count <= 0) {
+    count--;
+    if (count === 0) {
         clearInterval(counter);
 
     }
@@ -21,61 +25,56 @@ function timer() {
 }
 
 
-// function run() {
-//     clearInterval(intervalId);
-//     intervalId = setInterval(decrement, 1000);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 $(document).ready(function () {
 
+    //  does not seem to be working 
+    // this is the onlick fuction
+    // for the form
+    $("#submition").on("click", function (event) {
+        event.preventDefault();
+        console.log("this button has been click");
+        var answer1 = $("input[name=Question1]:checked").val();
+        var answer2 = $("input[name=Question2]:checked").val();
+        var answer3 = $("input[name=Question3]:checked").val();
+        var answer4 = $("input[name=Question4]:checked").val();
+        var answer5 = $("input[name=Question5]:checked").val();
+        var answer6 = $("input[name=Question6]:checked").val();
+
+console.log("answer1 " + answer1);
+        // checking if answers are correct/or incorrect
+        if ("correct" === answer1) {
+            console.log("you are correct");
+            correct++;
+        }
+        if ("correct" === answer2) {
+            console.log("you are correct");
+            correct++;
+        }
+        if ("correct" === answer2) {
+            console.log("you are correct");
+            correct++;
+        }
+        if ("correct" === answer3) {
+            console.log("you are correct");
+            correct++;
+        }
+        if ("correct" === answer4) {
+            console.log("you are correct");
+            correct++;
+        }
+        if ("correct" === answer5) {
+            console.log("you are correct");
+            correct++;
+        }
 
 
 
 
-
-
-
-
-
+    });
+});
+// on click of start button this should start the quiz
+$("#start-game").on("click", function(){
+    $("#trivia-quiz").show();
+    $("#start-game").hide();
 })
-
-
-
-
-// does not seem to be working 
-// this is the onlick fuction for the form
-//     $("#submition").on("click", function () {
-//         event.preventDefault();
-//         var answer1 = $("input[name=question1]:checked").val();
-//         var answer2 = $("input[name=question2]:checked").val();
-//         var answer3 = $("input[name=question3]:checked").val();
-//         var answer4 = $("input[name=question4]:checked").val();
-//         var answer5 = $("input[name=question5]:checked").val();
-//         var answer6 = $("input[name=question6]:checked").val();
-
-//         console.log("answer1");
-//         console.log("answer2");
-
-//         if("correct" === answer1){
-//             console.log("you are correct");
-//         }
-
-
-//     });
-
